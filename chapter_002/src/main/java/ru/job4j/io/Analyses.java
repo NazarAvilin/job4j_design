46327847b9da7b00ae7cc373d57fc95c87b2a75c
NazarAvilin <avilin1@outlook.com>
2020-08-05 21:17:34 +0500

    2. Анализ доступности сервера. [#316701] Раильвский метод

---
+++ chapter_002/src/main/java/ru/job4j/io/Analyses.java
@@ -0,0 +1,51 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+
+public class Analyses {
+
+    private static final String NORMAL_STATUS = "[123].+";
+
+    public void unavailable(String source, String target) {
+        try (BufferedReader br = new BufferedReader(new FileReader(source));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(target))) {
+            boolean fallen = false;
+            String start = "";
+            String line;
+            do {
+                line = br.readLine();
+                if (line != null) {
+                    boolean isNormal = line.matches(NORMAL_STATUS);
+                    if (!fallen && !isNormal && line.startsWith("500")) {
+                        start = line.split(" ")[1];
+                        fallen = true;
+                    } else if (!fallen && !isNormal && line.startsWith("400")) {
+                        start = line.split(" ")[1];
+                        fallen = true;
+                    } else if (fallen && isNormal) {
+                        bw.write(String.format("%s;%s%n", start, line.split(" ")[1]));
+                        fallen = false;
+                    }
+                }
+            } while (line != null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readResult(String path) {
+        StringJoiner rsl = new StringJoiner(System.lineSeparator());
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines().forEach(rsl::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rsl.toString();
+    }
+
+    public static void main(String[] args) {
+        Analyses analyses = new Analyses();
+        analyses.unavailable("server.txt", "unavailable.csv");
+    }
+}
--- chapter_002/src/test/java/ru/job4j/io/AnalysesTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalysesTest.java
@@ -1,62 +1,62 @@
-//package ru.job4j.io;
-//
-//import java.io.*;
-//import org.junit.*;
-//import java.util.*;
-//import static org.junit.Assert.*;
-//import java.nio.file.Files;
-//import org.junit.rules.TemporaryFolder;
-//import static org.hamcrest.core.Is.is;
-//
-//
-//public class AnalysesTest {
-//
-//    @Test
-//    public void whenFindUnavailableDiapasons() {
-//        String serverLog = "./data/server.txt";
-//        Analyses anls = new Analyses();
-//        anls.unavailable(serverLog, "./data/unavailable.csv");
-//        String result = anls.readResult("./data/unavailable.csv");
-//        assertThat(result, is(new StringJoiner(System.lineSeparator())
-//                .add("10:57:01;10:59:01")
-//                .add("11:01:02;11:02:02").toString()));
-//    }
-//
-//    @Rule
-//    public TemporaryFolder folder = new TemporaryFolder();
-//
-//    @Test
-//    public void whenReadFromTemporaryFile() throws IOException {
-//        Analyses an = new Analyses();
-//        File source = folder.newFile("source.txt");
-//        File target = folder.newFile("target.txt");
-//        Files.write(source.toPath(), List.of("200 10:56:01",
-//                                            "500 10:57:01",
-//                                            "400 10:58:01",
-//                                            "200 10:59:01",
-//                                            "500 11:01:02",
-//                                            "200 11:02:02"));
-//        an.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
-//        List<String> expect = List.of("10:57:01;10:59:01", "11:01:02;11:02:02");
-//        List<String> result = Files.readAllLines(target.toPath());
-//        assertThat(result, is(expect));
-//    }
-//
-//    @Test
-//    public void whenReadFromTemporaryFileOtherExample() throws IOException {
-//        Analyses an = new Analyses();
-//        File source = folder.newFile("source.txt");
-//        File target = folder.newFile("target.txt");
-//        Files.write(source.toPath(), List.of("200 10:56:01",
-//                                            "200 10:57:01",
-//                                            "400 10:58:01",
-//                                            "200 10:59:01",
-//                                            "500 11:01:02",
-//                                            "200 11:02:02"));
-//        an.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
-//        List<String> expect = List.of("10:58:01;10:59:01", "11:01:02;11:02:02");
-//        List<String> result = Files.readAllLines(target.toPath());
-//        assertThat(result, is(expect));
-//    }
-//
-//}
+package ru.job4j.io;
+
+import java.io.*;
+import org.junit.*;
+import java.util.*;
+import static org.junit.Assert.*;
+import java.nio.file.Files;
+import org.junit.rules.TemporaryFolder;
+import static org.hamcrest.core.Is.is;
+
+
+public class AnalysesTest {
+
+    @Test
+    public void whenFindUnavailableDiapasons() {
+        String serverLog = "./data/server.txt";
+        Analyses anls = new Analyses();
+        anls.unavailable(serverLog, "./data/unavailable.csv");
+        String result = anls.readResult("./data/unavailable.csv");
+        assertThat(result, is(new StringJoiner(System.lineSeparator())
+                .add("10:57:01;10:59:01")
+                .add("11:01:02;11:02:02").toString()));
+    }
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenReadFromTemporaryFile() throws IOException {
+        Analyses an = new Analyses();
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        Files.write(source.toPath(), List.of("200 10:56:01",
+                                            "500 10:57:01",
+                                            "400 10:58:01",
+                                            "200 10:59:01",
+                                            "500 11:01:02",
+                                            "200 11:02:02"));
+        an.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        List<String> expect = List.of("10:57:01;10:59:01", "11:01:02;11:02:02");
+        List<String> result = Files.readAllLines(target.toPath());
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenReadFromTemporaryFileOtherExample() throws IOException {
+        Analyses an = new Analyses();
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        Files.write(source.toPath(), List.of("200 10:56:01",
+                                            "200 10:57:01",
+                                            "400 10:58:01",
+                                            "200 10:59:01",
+                                            "500 11:01:02",
+                                            "200 11:02:02"));
+        an.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        List<String> expect = List.of("10:58:01;10:59:01", "11:01:02;11:02:02");
+        List<String> result = Files.readAllLines(target.toPath());
+        assertThat(result, is(expect));
+    }
+
+}
