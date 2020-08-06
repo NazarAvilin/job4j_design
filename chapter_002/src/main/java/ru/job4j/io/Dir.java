c3ff405a210e6569b57bc70889be67fc29700dd9
NazarAvilin <avilin1@outlook.com>
2020-08-06 19:56:30 +0500

    5. Валидация параметров запуска. [#316693]

--- chapter_002/src/main/java/ru/job4j/io/Dir.java
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -5,17 +5,21 @@ import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\Projects");
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
         System.out.printf("size : %s%n", file.getTotalSpace());
         for (File subFile : Objects.requireNonNull(file.listFiles())) {
             System.out.printf("name:%s size:%d KB%n",
                     subFile.getName(), (long) Math.ceil(subFile.length() / 1024.0));
         }
+
     }
 }
--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -11,8 +11,11 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null or file type in not mentioned");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
