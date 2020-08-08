ccf09ab79b9d9ccea6253c1a292c37c3d8c95987
NazarAvilin <avilin1@outlook.com>
2020-08-08 18:39:46 +0500

    new update

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -3,16 +3,21 @@ package ru.job4j.io;
 import java.io.FileOutputStream;
 
 public class ResultFile {
-    public static void main(String[] args) {
+
+    public static void multiplyTable(int step) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for (int i = 1; i <= 10; i++) {
-                out.write(System.lineSeparator().getBytes());
-                for (int j = 1; j <= 10; j++) {
-                    out.write((i + " * " + j + " = " + i * j).getBytes());
+            for (int i = 1; i <= step; i++) {
+                for (int j = 1; j <= step; j++) {
+                    out.write((i + " x " + j + " = " + (i * j) + System.lineSeparator()).getBytes());
                 }
+                out.write(System.lineSeparator().getBytes());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        multiplyTable(12);
     }
 }
