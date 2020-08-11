0789a0d63c20a4dc260a49a1170fb4b11187df08
NazarAvilin <avilin1@outlook.com>
2020-08-11 12:26:13 +0500

    4.2. Поиск дубликатов [#316699]

--- chapter_002/src/main/java/ru/job4j/io/FileGetter.java
+++ chapter_002/src/main/java/ru/job4j/io/FileGetter.java
@@ -5,52 +5,45 @@ import java.nio.file.*;
 import java.nio.file.FileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
-import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class FileGetter implements FileVisitor<Path> {
-    private final List<String> source = new ArrayList<>();
-
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-        return CONTINUE;
+        return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-        List<Path> metFiles = new ArrayList<>();
-        List<Path> duplicates = new LinkedList<>();
-        source.add(String.valueOf(file.toAbsolutePath()));
-        for (var el:source) {
-            metFiles.add(Path.of(el + System.lineSeparator()));
-        }
-        for (int i = 0; i < metFiles.size(); i++) {
-            Path el1 = metFiles.get(i).getFileName();
+        Path source = file.toAbsolutePath();
+        Set<Path> duplicates = new HashSet<>();
+        List<Path> list = new ArrayList<>();
+        List<Path> temp = new ArrayList<>();
+        temp.add(source);
+        list.add(source.getFileName());
+        for (int i = 0; i < list.size(); i++) {
+            Path el1 = list.get(i);
             if (el1 == null) {
                 continue;
             }
-            for (int j = 0; j < metFiles.size(); j++) {
-                if (i == j) {
-                    continue;
-                }
-                Path el2 = metFiles.get(j).getFileName();
+            for (int j = 0; j < list.size(); j++) {
+                Path el2 = list.get(j);
                 if (el1.equals(el2)) {
-                    duplicates.add(Path.of(source.get(j) + System.lineSeparator()));
+                    duplicates.add(temp.get(j));
                 }
             }
         }
-        System.out.println("Source \n" + source);
-        System.out.println("MetFiles \n" + metFiles);
-        System.out.println("Duplicates \n" + duplicates);
-        return CONTINUE;
+        System.out.println(duplicates);
+
+        return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) {
-        return CONTINUE;
+        return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
-        return CONTINUE;
+        return FileVisitResult.CONTINUE;
     }
 }
